d5189d781e101e8e6f090ebfa123ade038aa3e90
Its_Shojib <mdshojib922@gmail.com>
2023-12-17 01:54:43 +0600

    Chatbot issue fixed

--- src/Pages/Home/ChatBot.jsx
+++ src/Pages/Home/ChatBot.jsx
@@ -3,8 +3,8 @@
 const ChatBot = () => {
     return (
         <div className=' hidden md:flex flex-col bottom-[0%] right-0 fixed max-w-screen-xl z-50 gap-2'>
-            <img className="w-12 cursor-pointer" src="/public/chat.png" alt="" />
-            <img className="w-12 cursor-pointer" src="/public/whatsapp.png" alt="" />
+            <img className="w-12 cursor-pointer" src="/chat.png" alt="" />
+            <img className="w-12 cursor-pointer" src="/whatsapp.png" alt="" />
         </div>
     )
 }
